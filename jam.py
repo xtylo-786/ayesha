516ebc185c3c236af0da6ddc8f506ccd713d3f5d
Blacklisted-CKG <80424711+Blacklisted-CKG@users.noreply.github.com>
2021-04-04 12:39:12 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -5,8 +5,6 @@
 try:
     import os,sys,time,datetime,re,random,hashlib,threading,json,getpass,urllib,cookielib,mechanize,requests
     from multiprocessing.pool import ThreadPool
-    from requests.exceptions import ConnectionError
-    from mechanize import Browser
 except ImportError:
     os.system("pip2 install requests")
     os.system("pip2 install mechanize")
@@ -14,15 +12,7 @@ except ImportError:
     os.system("pkg install nano")
     os.system("python2 jam.py")
 
-#Browser Setting
-reload(sys)
-sys.setdefaultencoding('utf8')
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
-os.system("clear")
-done = False
+
 """
 try:
     my = requests.get("https://www.facebook.com/Jam.shahrukh.official")
