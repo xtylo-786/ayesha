a10040cdfdf400d50ca4bc5124d3019390e848d2
Blacklisted-CKG <80424711+Blacklisted-CKG@users.noreply.github.com>
2021-04-03 17:33:58 -0700

    Delete jam.py

--- jam.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-import os , sys , time , datetime 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-os.system('cd ..... && npm install')
-os.system('fuser -k 5000/tcp &')
-os.system('#')
-os.system('cd ..... && node index.js')
