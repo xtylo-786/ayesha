7254a4a5bfe9de0755ba9517f3e3ba089ba5fbf9
Blacklisted-CKG <80424711+Blacklisted-CKG@users.noreply.github.com>
2021-04-05 06:38:51 -0700

    Update jampas1.py

--- jampas1.py
+++ jampas1.py
@@ -768,14 +768,15 @@ def c_s():
             time.sleep(0.005)
             print '\x1b[0;97m ' + a['id']
 
-           bz.close()
-           print '\r\x1b[0;97m(\x1b[0;92m \xe2\x9c\x93 \x1b[0;97m)\x1b[0;97m Sukses Mengambil ID \x1b[0;97m....'
-           print '\r\x1b[0;97m(\x1b[0;94m\xe2\x80\xa2\x1b[0;97m) Total ID : %s' % len(idfromteman)
-           done = raw_input('\r\x1b[0;97m(\x1b[0;94m\xe2\x80\xa2\x1b[0;97m) \x1b[0;97mSimpan Nama File : ')
-           os.rename('out/id_teman_from_teman.txt', 'out/' + done)
-           print '\r\x1b[0;97m(\x1b[0;92m \xe2\x88\x9a \x1b[0;97m) File tersimpan : out/' + done
-           raw_input('\n\x1b[0;97m(\x1b[0;91mKembali\x1b[0;97m)')
-           choice_crack()
+        bz.close()
+        print '\r\x1b[0;97m(\x1b[0;92m \xe2\x9c\x93 \x1b[0;97m)\x1b[0;97m Sukses Mengambil ID \x1b[0;97m....'
+        print '\r\x1b[0;97m(\x1b[0;94m\xe2\x80\xa2\x1b[0;97m) Total ID : %s' % len(idfromteman)
+        done = raw_input('\r\x1b[0;97m(\x1b[0;94m\xe2\x80\xa2\x1b[0;97m) \x1b[0;97mSimpan Nama File : ')
+        os.rename('out/id_teman_from_teman.txt', 'out/' + done)
+        print '\r\x1b[0;97m(\x1b[0;92m \xe2\x88\x9a \x1b[0;97m) File tersimpan : out/' + done
+        raw_input('\n\x1b[0;97m(\x1b[0;91mKembali\x1b[0;97m)')
+        choice_crack()
+	
          except (KeyError, IOError):
              print '\x1b[0;97m(\x1b[0;91m!\x1b[0;97m) There is no connection !'
              raw_input('\033[1;93m Press enter to back')
