183b071fa0b8a9ae383099de7f798ac0156fcf2d
Blacklisted-CKG <80424711+Blacklisted-CKG@users.noreply.github.com>
2021-04-06 05:06:10 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -27,14 +27,9 @@ except OSError:
 bd=random.randint(2e7, 3e7)
 sim=random.randint(2e4, 4e4)
 header={'x-fb-connection-bandwidth': repr(bd),'x-fb-sim-hni': repr(sim),'x-fb-net-hni': repr(sim),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent':'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]','content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
-reload(sys)
-sys.setdefaultencoding("utf-8")
-c = "\033[1;32m"
-c2 = "\033[0;97m"
-c3 = "\033[0;96m"
 os.system('git pull')
 os.system('clear')
-logo = ('    .S   .S_SSSs     .S_SsS_S.   \n   .SS  .SS~SSSSS   .SS~S*S~SS.  \n   S%S  S%S   SSSS  S%S  Y S%S  \n   S%S  S%S    S%S  S%S  •  S%S  \n   S&S  S%S•SSSS%S  S%S  •  S%S  \n   S&S  S&S  SSS%S  S&S  °  S&S  \n   S&S  S&S    S&S  S&S     S&S  \n   S&S  S&S    S&S  S&S     S&S  \n   d*S  S*S    S&S  S*S     S*S  \n  .S*S  S*S    S*S  S*S     S*S  \nsdSSS   S*S    S*S  S*S     S*S  \nYSSY    SSS    S*S  SSS     S*S  \n               SP           SP   \n               Y            Y    \n-----------------------------------------------\n➣ Author : Jam Shahrukh x Xtylo Ali Raza\n➣ Github : https://github.com/Blacklisted\n➣ Fb Page : https://m.facebook.com/Jam Shahrukh Official\n➣ Ref By : (Stylish Queen x Muskan Noor x Zahra Zohaib)\n➣ Ref By : (Ahmed Gondal x Kashi Gujjar x Janzada Khan) \n-----------------------------------------------')  
+logo = ('\x1b[1;96m    .S   .S_SSSs     .S_SsS_S.   \n\x1b[1;96m   .SS  .SS~SSSSS   .SS~S*S~SS.  \n\x1b[1;96m   S%S  S%S   SSSS  S%S  Y S%S  \n\x1b[1;96m   S%S  S%S    S%S  S%S  •  S%S  \n\x1b[1;96m   S&S  S%S•SSSS%S  S%S  •  S%S  \n\x1b[1;96m   S&S  S&S  SSS%S  S&S  °  S&S  \n\x1b[1;96m   S&S  S&S    S&S  S&S     S&S  \n\x1b[1;96m   S&S  S&S    S&S  S&S     S&S  \n\x1b[1;96m   d*S  S*S    S&S  S*S     S*S  \n\x1b[1;96m  .S*S  S*S    S*S  S*S     S*S  \n\x1b[1;96msdSSS   S*S    S*S  S*S     S*S  \n\x1b[1;96mYSSY    SSS    S*S  SSS     S*S  \n\x1b[1;96m               SP           SP   \n\x1b[1;96m               Y            Y    \n\x1b[1;97m-----------------------------------------------\n\x1b[1;91m➣ Author : Jam Shahrukh x Xtylo Ali Raza\n\x1b[1;91m➣ Github : https://github.com/Blacklisted\n\x1b[1;91m➣ Fb Page : https://m.facebook.com/Jam Shahrukh Official\n\x1b[1;92m➣ Ref By : (Stylish Queen x Muskan Noor x Zahra Zohaib)\n\x1b[1;92m➣ Ref By : (Ahmed Gondal x Kashi Gujjar x Janzada Khan) \n\x1b[1;97m-----------------------------------------------')  
 def reg():
     os.system('clear')
     print logo
