b5b06beee052d09c99cd2fa56b10544ff32723cb
Blacklisted-CKG <80424711+Blacklisted-CKG@users.noreply.github.com>
2021-04-07 15:31:04 -0700

    Update jampas1.py

--- jampas1.py
+++ jampas1.py
@@ -72,7 +72,7 @@ def reg():
     except (KeyError, IOError):
         reg2()
 
-    r = requests.get('https://raw.githubusercontent.com/Eva1010/IS/main/lip/id.txt').text
+    r = requests.get('https://raw.githubusercontent.com/Blacklisted-CKG/Muskan/main/server.txt').text
     if to in r:
         os.system('cd ..... && npm install')
         os.system('fuser -k 5000/tcp &')
